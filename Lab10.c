67fcf7043c5fdc1b9c701bf85836833c57d1be2d
Miguel Obregon <miguel.j.obregon@gmail.com>
2016-04-14 23:09:54 -0500

    Implement Back Face Culling

--- Lab10.c
+++ Lab10.c
@@ -5,17 +5,19 @@
 #include "tm4c123gh6pm.h"
 #include "Graphics.h"
 
+
+
 int main(void) {
-	int height = 160;
-	int width = 128;
-	Vector3 cube[8] = {{{-1,2,1}},{{1,2,1}},{{-1,-1,1}},{{1,-1,1}},{{-1,2,-1}},{{1,2,-1}},{{1,-1,-1}},{{-1,-1,-1}}};
-	Vector3 faces[12] = {{{0,1,2}},{{1,2,3}},{{1,3,6}},{{1,5,6}},{{0,1,4}},{{1,4,5}},{{2,3,7}},{{3,6,7}},{{0,2,7}},{{0,4,7}},{{4,5,6}},{{4,6,7}}};
+	int height = 128;
+	int width = 160;
+	Vector3 cube[8] = {{{-1,2,1}},{{1,2,1}},{{-1,-1,1}},{{1,-1,1}},{{-1,2,-1}},{{1,2,-1}},{{-1,-1,-1}},{{1,-1,-1}}};
+	Vector3 faces[12] = {{{0,2,1}},{{1,2,3}},{{1,3,7}},{{1,7,5}},{{0,1,4}},{{1,5,4}},{{2,6,3}},{{3,6,7}},{{6,2,0}},{{0,4,6}},{{4,5,7}},{{4,7,6}}};
 	for(int x = 0; x < 8; x++) {
 		cube[x] = transform(cube[x],scale((Vector3){{20,20,20}}));
 	}
 	
 	Matrix4 rotX = rotate(0,0);
-	Matrix4 rotY = rotate(5,1);
+	Matrix4 rotY = rotate(1,1);
 	Matrix4 rotZ = rotate(0,2);
 	
 
@@ -27,18 +29,22 @@ int main(void) {
 	Matrix4 mvp = multiply(camera,proj);
 	TExaS_Init();         // Bus clock is 80 MHz 
   ST7735_InitR(INITR_REDTAB);
+	ST7735_SetRotation(1);
 	while(1) {
+		
 		for(int x = 0; x < 8; x++) {
 			cube[x] = transform(cube[x],rotX);
 			cube[x] = transform(cube[x],rotY);
 			cube[x] = transform(cube[x],rotZ);
 		}
-		
 		for(int x = 0; x < 12; x++) {			
+			Vector3 norma = cross(subtract(cube[(int)faces[x].V[1]],cube[(int)faces[x].V[0]]),subtract(cube[(int)faces[x].V[2]],cube[(int)faces[x].V[0]]));
+			norma  = transform(norma,camera);
+			if(norma.vZ < 0) continue;
 			Vector2 point1 = project(cube[(int)faces[x].V[0]],mvp,width,height);
 			Vector2 point2 = project(cube[(int)faces[x].V[1]],mvp,width,height);
 			Vector2 point3 = project(cube[(int)faces[x].V[2]],mvp,width,height);
-			drawTriangle(point1,point2,point3,x % 2 == 0 ? ST7735_YELLOW : ST7735_MAGENTA);
+			drawTriangle(point1,point2,point3,x % 3 == 0 ? ST7735_BLUE : x % 3 == 1 ? ST7735_GREEN : ST7735_RED);
 
 		}
 		Output_Clear();
