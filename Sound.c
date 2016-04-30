66ad6580adba8156c510a7bbb2f3eb1700d0fb35
Miguel Obregon <miguel_lego@yahoo.com>
2016-04-29 19:06:05 -0500

    Fix sound project file

--- Sound.c
+++ Sound.c
@@ -1,9 +1,4 @@
 // Sound.c
-// Runs on any computer
-// Sound assets based off the original Space Invaders 
-// Import these constants into your SpaceInvaders.c for sounds!
-// Jonathan Valvano
-// November 17, 2014
 #include <stdint.h>
 #include "Sound.h"
 #include "DAC.h"
